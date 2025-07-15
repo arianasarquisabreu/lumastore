6ff78cd139dcd7273f78202af823120d79b783a1
arianasarquisabreu <166823262+arianasarquisabreu@users.noreply.github.com>
2025-07-15 13:26:41 -0300

    Update luma-store.cy.js

--- lumastore/cypress/e2e/luma-store.cy.js
+++ lumastore/cypress/e2e/luma-store.cy.js
@@ -1,5 +1,5 @@
 describe('Luma Store - Testes E2E', () => {
-  it('Verifica se a home page carrega corretamente', () => {
+  it('Verifica se a home está sendo carregada corretamente', () => {
     cy.visit('https://magento.softwaretestingboard.com');
     cy.get('body').should('be.visible');
     cy.get('img[alt="Luma logo"]').should('be.visible');
